0ab10f331f5839a787b32e961ad6b1fd8eec8e37
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-10-21 15:21:37 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
 basic.forever(function () {
     for (let i = 0; i <= 4; i++) {
         for (let j = 0; j <= 4; j++) {
-            if (i % 2 == 0 && j % 2 == 0) {
-                led.plot(i, j)
-            } else if (i % 2 == 1 && j % 2 == 1) {
+            if (i % 2 == j % 2) {
                 led.plot(i, j)
             }
-            basic.pause(100)
         }
     }
-    basic.clearScreen()
 })
